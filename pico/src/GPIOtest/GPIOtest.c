4a175b651adc78f0b8a100203d0894308e316c48
Tsukasa Takahashi <z19083z8@shibaurafzk.com>
2023-08-25 20:05:13 +0900

    Add GPIOtest/GPIOtest.c file
    GPIO 0~22, 26~28をすべて順に点灯させるやつ

--- pico/src/GPIOtest/GPIOtest.c
+++ pico/src/GPIOtest/GPIOtest.c
@@ -1,23 +1,35 @@
-/**
- * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
-
 #include "pico/stdlib.h"
 
 int main() {
 #ifndef PICO_DEFAULT_LED_PIN
-#warning blink example requires a board with a regular LED
 #else
     const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
+
+    int i;
+    uint LED_PIN_TMP;
+    for (i=0; i<=28; i++) {
+        LED_PIN_TMP = i;
+        if (i <= 22 || i >= 26) {
+            gpio_init(LED_PIN_TMP);
+            gpio_set_dir(LED_PIN_TMP, true);
+        }
+    }
+
     while (true) {
-        gpio_put(LED_PIN, 1);
-        sleep_ms(250);
-        gpio_put(LED_PIN, 0);
-        sleep_ms(250);
+        for (i=0; i<=28; i++) {
+            LED_PIN_TMP = i;
+            if (i <= 22 || i >= 26) {
+                gpio_put(LED_PIN_TMP, 1);
+                gpio_put(LED_PIN, 1);
+                sleep_ms(150);
+                gpio_put(LED_PIN_TMP, 0);
+                gpio_put(LED_PIN, 0);
+                sleep_ms(150);
+            }
+        }
     }
 #endif
 }
